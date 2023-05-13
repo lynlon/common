ff40f0e0e2ecfb3cf8265b564623b57e0e2f3a66
lynlon <leixiandeng@126.com>
2023-05-13 14:45:00 +0800

    Update get_sources.sh

--- get_sources.sh
+++ get_sources.sh
@@ -6,5 +6,6 @@ while read line
 do
   pkg=`echo $line|awk '{print $2}'`
   pkgchecksum=`echo $line|awk '{print $1}'`
-  wget -O ${pkg} ${sourceUrl}/${pkg}.${pkgchecksum}
+  #wget -O ${pkg} ${sourceUrl}/${pkg}.${pkgchecksum}
+  curl -o ${pkg} ${sourceUrl}/${pkg}.${pkgchecksum}
 done < download

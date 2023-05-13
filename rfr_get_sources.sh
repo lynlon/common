6c4d89c4ffa3eb13414790d1bb678a3ccdcbe484
lynlon <leixiandeng@126.com>
2023-05-13 14:03:58 +0800

    Update rfr_get_sources.sh

--- rfr_get_sources.sh
+++ rfr_get_sources.sh
@@ -9,7 +9,7 @@ fi
 if [ -f ./get_sources.sh ] && [ -f ./download ]; then
     cp ../common/get_sources.sh .
     sh ./get_sources.sh
-    rm -f get_sources.sh
+    rm -f ./get_sources.sh
 elif [ -x /usr/bin/spectool ]; then
     RUNSPECTOOL="no"
     for i in `spectool -l -S *.spec | awk -F"[ /]" '{ print $NF }'`; do

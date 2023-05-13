d0dc8a8146f6224b249855a12e6c8794822cda97
lynlon <leixiandeng@126.com>
2023-05-13 13:45:20 +0800

    Create rfr_get_sources.sh

---
+++ rfr_get_sources.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+LANG=C
+
+if [ ! -f *.spec ]; then
+    echo "Cannot find spec file"
+    exit 2
+fi
+
+if [ -f ./get_sources.sh ] && [ -f ./download ]; then
+    sh ./get_sources.sh
+elif [ -x /usr/bin/spectool ]; then
+    RUNSPECTOOL="no"
+    for i in `spectool -l -S *.spec | awk -F"[ /]" '{ print $NF }'`; do
+        if [ ! -f $i ]; then
+             echo NO $i found;
+             RUNSPECTOOL="yes"
+             break;
+        fi
+    done
+
+    if [ "$RUNSPECTOOL" == "yes" ]; then
+        /usr/bin/spectool -g *.spec
+    else
+        echo "All sources found"
+    fi
+else
+    echo "Cannot download source. No get_source.sh nor spectool found."
+    exit 1
+fi
